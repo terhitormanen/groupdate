7220d0dfbdf1a0392be93cc02e4c650198151dd2
Terhi Törmänen <terhi.tormanen@gmail.com>
2021-03-28 15:17:50 +0300

    Remove secret

--- test/adapters/sqlserver.rb
+++ test/adapters/sqlserver.rb
@@ -3,4 +3,4 @@ ActiveRecord::Base.establish_connection(adapter: "sqlserver",
     port: 1433,
     database: "groupdate_test",
     username: "test_adm1",
-    password: "52SuistokainenAslan&24eQ&")
+    password: "*****")
